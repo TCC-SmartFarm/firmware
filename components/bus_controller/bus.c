82c457cf570624c3c3c44b381e2017472e216ffc
bruno-gia <b_gia@hotmail.com>
2026-03-23 18:58:25 -0300

    feat: configuracao do barramento I²C

--- components/bus_controller/bus.c
+++ components/bus_controller/bus.c
@@ -12,6 +12,8 @@ Arquivo destinado a realizar a configuração dos barramentos SPI e I2C a serem 
 
 static const char *TAG = "bus";
 
+
+// ----------------------------------------------------------------- SPI -----------------------------------------------------------------
 esp_err_t bus_spi_init(spi_host_device_t host_id, int mosi_pin, int miso_pin, int sclk_pin, int max_transfer_sz) {
     // Configuração física do barramento
     spi_bus_config_t bus_config = {
@@ -37,6 +39,7 @@ esp_err_t bus_spi_init(spi_host_device_t host_id, int mosi_pin, int miso_pin, in
 }
 
 esp_err_t bus_spi_free(spi_host_device_t host_id) {
+
     // Libera os recursos alocados para o barramento
     esp_err_t ret = spi_bus_free(host_id);
     if (ret != ESP_OK) {
@@ -45,5 +48,48 @@ esp_err_t bus_spi_free(spi_host_device_t host_id) {
     }
 
     ESP_LOGI(TAG, "Barramento SPI liberado com sucesso.");
+    return ESP_OK;
+}
+
+// ----------------------------------------------------------------- I²C -----------------------------------------------------------------
+
+esp_err_t bus_i2c_init(i2c_port_t i2c_num, int sda_pin, int scl_pin, uint32_t clk_speed) {
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = sda_pin,
+        .scl_io_num = scl_pin,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = clk_speed,
+    }; // Configuração inicial do barramento I2C
+
+    ESP_LOGI(TAG, "\n ========== Inicializando I²C em: %d ========== \n", i2c_num);
+
+    // Parâmetros do periférico
+    esp_err_t ret = i2c_param_config(i2c_num, &conf);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "\n ========== Erro na inicialização! ========== \n Trace: (%s) \n", esp_err_to_name(ret));
+        return ret;
+    }
+
+    // Instalação do driver como Master (3 0's no final)
+    ret = i2c_driver_install(i2c_num, conf.mode, 0, 0, 0);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "\n ========== Erro na instalação do driver! ========== \n Trace: (%s) \n", esp_err_to_name(ret));
+        return ret;
+    }
+
+    ESP_LOGI(TAG, "\n ========== I²C OK! ========== \n");
+    return ESP_OK;
+}
+
+esp_err_t bus_i2c_free(i2c_port_t i2c_num) {
+    esp_err_t ret = i2c_driver_delete(i2c_num);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "\n ========== Erro na liberação do driver em %d! ========== \n Trace: (%s) \n)", i2c_num, esp_err_to_name(ret));
+        return ret;
+    }
+
+    ESP_LOGI(TAG, "\n ========== I²C Liberado! ========== \n");
     return ESP_OK;
 }
--- components/bus_controller/bus.h
+++ components/bus_controller/bus.h
@@ -11,10 +11,17 @@ Arquivo contendo os headers das funções implementadas no arquivo bus.c
 // Includes
 #include "esp_log.h"
 #include "driver/sdspi_host.h"
+#include "driver/i2c.h"
 #include "hal/spi_types.h"
 
+// Implementação do protocolo SPI
 esp_err_t bus_spi_init(spi_host_device_t host_id, int mosi_pin, int miso_pin, int sclk_pin, int max_transfer_sz);
 
 esp_err_t bus_spi_free(spi_host_device_t host_id);
+
+// Implementação do protocolo I²C
+esp_err_t bus_i2c_init(i2c_port_t i2c_num, int sda_pin, int scl_pin, uint32_t clk_speed);
+
+esp_err_t bus_i2c_free(i2c_port_t i2c_num);
 
 #endif 
